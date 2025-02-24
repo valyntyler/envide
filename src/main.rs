c01f883118e4b144604d8a5f33ddf3d21dad61f9
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-25 00:15:08 +0200

    feat: reading .env entries

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
+use envide::env::entry::Entry;
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(contents) = fs::read_to_string(".env") {
+        let entries: Vec<Entry> = contents
+            .lines()
+            .filter_map(|line| {
+                line.split_once('=').and_then(|(lhs, rhs)| {
+                    Some(Entry {
+                        key: lhs.to_string(),
+                        val: rhs.to_string()
+                    })
+                })
+            })
+            .collect();
+
+        for entry in entries {
+            println!("{}", entry);
+        }
+    }
 }
