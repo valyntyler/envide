4bce24d073ac205d61a81f0a742a7fb5a899db96
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-25 01:14:50 +0200

    feat: reading entry keys and values

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@ use std::fs;
 
 use envide::env::entry::Entry;
 
+pub fn get_value<'a>(key: &str, entries: &'a Vec<Entry>) -> Option<&'a str> {
+    for entry in entries {
+        if entry.key == key {
+            return Some(&entry.val)
+        }
+    }
+
+    None
+}
+
 fn main() {
     if let Ok(contents) = fs::read_to_string(".env") {
         let entries: Vec<Entry> = contents
@@ -9,8 +19,12 @@ fn main() {
             .filter_map(|line| line.try_into().ok())
             .collect();
 
-        for entry in entries {
+        for entry in &entries {
             println!("{}", entry);
+        }
+
+        if let Some(val) = get_value("JEFF", &entries) {
+            println!("{}", val);
         }
     }
 }
