f791128ff4475208eee82fff9e4990e21abcd2d8
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-25 00:41:41 +0200

    feat: impl entry string conversion

--- src/env/entry/mod.rs
+++ src/env/entry/mod.rs
@@ -1,9 +1,32 @@
 use core::fmt;
 
 #[derive(Debug)]
+pub enum ParseError {
+    NoEqualsSign,
+    EmptyKey,
+    EmptyVal,
+}
+
+#[derive(Debug)]
 pub struct Entry {
     pub key: String,
     pub val: String,
+}
+
+impl TryFrom<&str> for Entry {
+    type Error = ParseError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let (key, val) = value.split_once('=').ok_or(ParseError::NoEqualsSign)?;
+
+        if key.is_empty() { return Err(ParseError::EmptyKey)?; }
+        if val.is_empty() { return Err(ParseError::EmptyVal)?; }
+
+        Ok(Entry {
+            key: key.to_string(),
+            val: val.to_string()
+        })
+    }
 }
 
 impl fmt::Display for Entry {
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,7 @@ fn main() {
     if let Ok(contents) = fs::read_to_string(".env") {
         let entries: Vec<Entry> = contents
             .lines()
-            .filter_map(|line| {
-                line.split_once('=').and_then(|(lhs, rhs)| {
-                    Some(Entry {
-                        key: lhs.to_string(),
-                        val: rhs.to_string()
-                    })
-                })
-            })
+            .filter_map(|line| line.try_into().ok())
             .collect();
 
         for entry in entries {
