4f481bae272a12f283aac1beb63a99106b1620f9
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-25 01:32:10 +0200

    feat: taking path argument with clap

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@ use std::fs;
 
 use envide::env::entry::Entry;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = ".env")]
+    path: String,
+}
+
 pub fn get_value<'a>(key: &str, entries: &'a Vec<Entry>) -> Option<&'a str> {
     for entry in entries {
         if entry.key == key {
@@ -13,7 +22,9 @@ pub fn get_value<'a>(key: &str, entries: &'a Vec<Entry>) -> Option<&'a str> {
 }
 
 fn main() {
-    if let Ok(contents) = fs::read_to_string(".env") {
+    let args = Args::parse();
+
+    if let Ok(contents) = fs::read_to_string(args.path) {
         let entries: Vec<Entry> = contents
             .lines()
             .filter_map(|line| line.try_into().ok())
