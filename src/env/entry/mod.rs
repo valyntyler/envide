08482efe4d4bb6443712fa1afd6c038e9629ade8
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-24 23:32:21 +0200

    feat: impl display for entry

--- src/env/entry/mod.rs
+++ src/env/entry/mod.rs
@@ -1,5 +1,13 @@
+use core::fmt;
+
 #[derive(Debug)]
 pub struct Entry {
     pub key: String,
     pub val: String,
+}
+
+impl fmt::Display for Entry {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}={}", self.key, self.val)
+    }
 }
