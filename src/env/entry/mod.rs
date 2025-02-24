12ada12c03044759d608a2f7495c99c43ea2585d
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-25 00:47:34 +0200

    feat: entry no longer cloning values

--- src/env/entry/mod.rs
+++ src/env/entry/mod.rs
@@ -8,28 +8,28 @@ pub enum ParseError {
 }
 
 #[derive(Debug)]
-pub struct Entry {
-    pub key: String,
-    pub val: String,
+pub struct Entry<'a> {
+    pub key: &'a str,
+    pub val: &'a str,
 }
 
-impl TryFrom<&str> for Entry {
+impl<'a> TryFrom<&'a str> for Entry<'a> {
     type Error = ParseError;
 
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
+    fn try_from(value: &'a str) -> Result<Self, Self::Error> {
         let (key, val) = value.split_once('=').ok_or(ParseError::NoEqualsSign)?;
 
         if key.is_empty() { return Err(ParseError::EmptyKey)?; }
         if val.is_empty() { return Err(ParseError::EmptyVal)?; }
 
         Ok(Entry {
-            key: key.to_string(),
-            val: val.to_string()
+            key,
+            val,
         })
     }
 }
 
-impl fmt::Display for Entry {
+impl<'a> fmt::Display for Entry<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}={}", self.key, self.val)
     }
