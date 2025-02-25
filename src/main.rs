8eee2547940e35e887f407dc1d0422d9a8c1d66b
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-25 04:08:02 +0200

    feat: managing .env vars

--- src/main.rs
+++ src/main.rs
@@ -9,37 +9,63 @@ use clap::Parser;
 struct Args {
     #[arg(short, long, default_value = ".env")]
     path: String,
+
+    #[arg(short, long, default_value_t = false)] new: bool,
+
+    #[arg(short = 'x', long, default_value_t = false)]
+    exclusive: bool,
     
-    #[clap(short, long, value_names = [ "KEY", "NEW_VAL" ])]
-    replace: Option<Vec<String>>,
+    #[arg(short, long, default_value_t = false)]
+    replace: bool,
+
+    #[arg(num_args = 2, value_names = [ "KEY", "VALUE" ])]
+    trailing: Vec<String>,
 }
 
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
-    if let Some(replace) = args.replace {
-        let key = &replace[0];
-        let new = &replace[1];
+    let key = &args.trailing[0];
+    let val = &args.trailing[1];
 
-        let content: String = fs::read_to_string(&args.path)?
+    let mut content: String = fs::read_to_string(&args.path)?;
+
+    if args.replace {
+        content = content
             .lines()
-            .map(|line| {
-                if let Ok(mut entry) = Entry::try_from(line) {
-                    if entry.key == key { entry.val = new; }
-                    format!("{}\n", entry)
-                } else {
-                    line.to_string()
+            .filter(|line| match Entry::try_from(*line) {
+                Err(_) => true,
+                Ok(entry) => match entry.key == key {
+                    true => false,
+                    false => true,
                 }
             })
-            .collect();
-
-        let mut file = OpenOptions::new()
-            .write(true)
-            .truncate(true)
-            .open(&args.path)?;
-
-        file.write_all(content.as_bytes())?;
+            .map(|line| format!("{}\n", line))
+            .collect()
     }
+
+    if args.new {
+        let mut ok = true;
+        if args.exclusive {
+            content.lines().for_each(|line| {
+                if let Ok(entry) = Entry::try_from(line) {
+                    if entry.key == key { ok = false; }
+                }
+            });
+        }
+
+        if ok {
+            let entry = Entry { key, val };
+            content = format!("{}{}\n", content, entry);
+        }
+    }
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(&args.path)?;
+
+    file.write_all(content.as_bytes())?;
 
     Ok(())
 }
