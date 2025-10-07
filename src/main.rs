2b8ba8f73b6671639cf9ecfa604dab0d1bd06ba1
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-10-07 22:55:25 +0200

    feat: init ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,24 @@
-use std::{
-    fs::{self, OpenOptions},
-    io::{self, Write},
-};
+use color_eyre::Result;
+use crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
 
-use envide::env::entry::Entry;
-
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    #[arg(short, long, default_value = ".env")]
-    path: String,
-
-    #[arg(short, long, default_value_t = false)]
-    new: bool,
-
-    #[arg(short = 'x', long, default_value_t = false)]
-    exclusive: bool,
-
-    #[arg(short, long, default_value_t = false)]
-    replace: bool,
-
-    #[arg(num_args = 2, value_names = [ "KEY", "VALUE" ])]
-    trailing: Vec<String>,
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
 }
 
-fn main() -> io::Result<()> {
-    let args = Args::parse();
-
-    let key = &args.trailing[0];
-    let val = &args.trailing[1];
-
-    let mut content: String = fs::read_to_string(&args.path)?;
-
-    if args.replace {
-        content = content
-            .lines()
-            .filter(|line| match Entry::try_from(*line) {
-                Err(_) => true,
-                Ok(entry) => match entry.key == key {
-                    true => false,
-                    false => true,
-                },
-            })
-            .map(|line| format!("{}\n", line))
-            .collect()
-    }
-
-    if args.new {
-        let mut ok = true;
-        if args.exclusive {
-            content.lines().for_each(|line| {
-                if let Ok(entry) = Entry::try_from(line) {
-                    if entry.key == key {
-                        ok = false;
-                    }
-                }
-            });
-        }
-
-        if ok {
-            let entry = Entry { key, val };
-            content = format!("{}{}\n", content, entry);
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
         }
     }
+}
 
-    let mut file = OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .open(&args.path)?;
-
-    file.write_all(content.as_bytes())?;
-
-    Ok(())
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
